4e89cc100465632cc01a7a4d1329ad0a03591c06
Brian Butterly <butterlybrian@gmail.com>
2021-07-06 15:27:51 -0500

    initial navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React from "react";
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <h2>hi</h2>
+    <Navbar/>
   );
 }
 
